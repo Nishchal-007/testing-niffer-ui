fe6c2362a3a00b9184af837a42e99a4cbb6042bc
Nishchal-007 <letscollabtogether10@gmail.com>
2021-05-23 10:44:27 +0530

    Started PNG Extraction

--- nifflerUITest/urls.py
+++ nifflerUITest/urls.py
@@ -14,8 +14,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls import url,include
 from django.urls import path
+from django.urls import re_path
+from django.views.static import serve
+
+from pngextraction import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.pngHomeView, name="png_Home")
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
--- pngextraction/views.py
+++ pngextraction/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def pngHomeView(request):
+    return render(request, "pngextraction/pngHome.html")
