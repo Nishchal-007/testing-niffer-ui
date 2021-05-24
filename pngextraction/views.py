60f2b086726eff92744dac84bdf8257aee13fec6
Nishchal-007 <letscollabtogether10@gmail.com>
2021-05-24 18:48:25 +0530

    Receiving Data from Website

--- pngextraction/views.py
+++ pngextraction/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def pngHomeView(request):
+    if(request.method == 'POST'):
+        dicom_home = request.POST['depth']
+        print(dicom_home)
     return render(request, "pngextraction/pngHome.html")
